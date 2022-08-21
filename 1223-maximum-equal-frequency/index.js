ec2c80bfcb21f627d9800a617eeace26384830a3
guangpengliu-vungle <guangpeng.liu@vungle.com>
2022-08-22 00:47:39 +0800

    added 1223

--- 1223-maximum-equal-frequency/index.js
+++ 1223-maximum-equal-frequency/index.js
@@ -6,51 +6,66 @@
 let nums
 nums = [2,2,1,1,5,3,3,5]
 nums = [1,1,1,2,2,2,3,3,3,4,4,4,5]
+nums = [100000, 100000]
+
+// const maxEqualFreq = function(nums) {
+//   const n = nums.length
+//   let count = new Array(100001).fill(0)
+//   let freq = new Array(100001).fill(0)
+//
+//   for (let i = 0; i < nums.length; i++) {
+//     count[nums[i]]++
+//     freq[count[nums[i]]]++
+//   }
+//
+//   for (let i = nums.length - 1; i > 0; i--) {
+//     if (count[nums[i]] * freq[count[nums[i]]] === i ) {
+//       return i + 1
+//     }
+//
+//     freq[count[nums[i]]]--
+//     count[nums[i]]--
+//
+//     if (count[nums[i - 1]] * freq[count[nums[i - 1]]] === i) {
+//       return i + 1
+//     }
+//   }
+//
+//   return 1
+// };
 
 const maxEqualFreq = function(nums) {
-  let count = new Map()
-  let freq = new Map()
-  let maxFreq = 0
+  const n = nums.length
+  const count = new Map()
+  const freq = new Map()
 
-  for (let i = 0; i < nums; i++) {
+  for (let i = 0; i < n; i++) {
     if (!count.has(nums[i])) {
       count.set(nums[i], 0)
     }
+    count.set(nums[i], count.get(nums[i]) + 1)
 
-    count.set(nums[i], count.get(nums[i] + 1))
-
-    maxFreq = Math.max(maxFreq, count.get(nums[i]));
-  }
-
-  console.log(count)
-};
-
-var maxEqualFreq23 = function(nums) {
-  const freq = new Map();
-  const count = new Map();
-  let res = 0, maxFreq = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (!count.has(nums[i])) {
-      count.set(nums[i], 0);
-    }
-    if (count.get(nums[i]) > 0) {
-      freq.set(count.get(nums[i]), freq.get(count.get(nums[i])) - 1);
-    }
-    count.set(nums[i], count.get(nums[i]) + 1);
-    maxFreq = Math.max(maxFreq, count.get(nums[i]));
     if (!freq.has(count.get(nums[i]))) {
-      freq.set(count.get(nums[i]), 0);
+      freq.set(count.get(nums[i]), 0)
     }
-    freq.set(count.get(nums[i]), freq.get(count.get(nums[i])) + 1);
-    const ok = maxFreq === 1 ||
-      freq.get(maxFreq) * maxFreq + freq.get(maxFreq - 1) * (maxFreq - 1) === i + 1 && freq.get(maxFreq) === 1 ||
-      freq.get(maxFreq) * maxFreq + 1 === i + 1 && freq.get(1) === 1;
-    if (ok) {
-      res = Math.max(res, i + 1);
+    freq.set(count.get(nums[i]), freq.get(count.get(nums[i])) + 1)
+  }
+
+  for (let i = n - 1; i > 0; i--) {
+    if (count.get(nums[i]) * freq.get(count.get(nums[i])) === i) {
+      return i + 1
+    }
+
+    freq.set(count.get(nums[i]), freq.get(count.get(nums[i])) - 1)
+    count.set(nums[i], count.get(nums[i]) - 1)
+
+    if (count.get(nums[i - 1]) * freq.get(count.get(nums[i - 1])) === i) {
+      return i + 1
     }
   }
-  return res;
-};
+
+  return 1
+}
 
 console.log(maxEqualFreq(nums))
 
