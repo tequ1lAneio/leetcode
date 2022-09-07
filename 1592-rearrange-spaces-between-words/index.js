d8fc340f6607cdac4bab8c61374a55f6afa4e517
tequ1lAneio <evanlgp@hotmail.com>
2022-09-07 21:35:57 +0800

    added 1592

---
+++ 1592-rearrange-spaces-between-words/index.js
@@ -0,0 +1,65 @@
+/**
+ * @param {string} text
+ * @return {string}
+ */
+
+let text
+text = "  this   is  a sentence "
+text = " practice   makes   perfect"
+// text = "hello   world"
+// text = "  walks  udp package   into  bar a"
+// text = "a"
+text = "  hello"
+
+// const reorderSpaces = (text) => {
+//   let spaceCount = 0
+//   let words = []
+//   let word = ''
+//   let ans = ''
+//
+//   for (const ele of text) {
+//     if (ele === ' ') {
+//       spaceCount++
+//       if (word) {
+//         words.push(word)
+//         word = ''
+//       }
+//     } else {
+//       word += ele
+//     }
+//   }
+//
+//   if (word) {
+//     words.push(word)
+//   }
+//
+//   let interval = ''
+//   const intervalLength = words.length > 1 ? Math.floor(spaceCount / (words.length - 1)) : 0
+//   for (let i = 0; i < intervalLength; i++) {
+//     interval += ' '
+//   }
+//
+//   let restSpace = ''
+//   const restSpaceLength = words.length > 1 ? spaceCount % (words.length - 1) : spaceCount
+//   for (let i = 0; i < restSpaceLength; i++) {
+//     restSpace += ' '
+//   }
+//
+//   for (let i = 0; i < words.length; i++) {
+//     if (i > 0) {
+//       ans += interval
+//     }
+//     ans += words[i]
+//   }
+//
+//   ans += restSpace
+//
+//   return ans
+// }
+
+// official
+
+
+
+
+console.log(reorderSpaces(text))
