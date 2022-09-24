f7752de216a4440424c70daf0c54bbd6640e24a6
tequ1lAneio <evanlgp@hotmail.com>
2022-09-24 17:28:17 +0800

    added 854

---
+++ 707-design-linked-list/linkedList.js
@@ -0,0 +1,107 @@
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
+
+var MyLinkedList = function() {
+  this.size = 0
+  this.head = new ListNode(0)
+};
+
+/**
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+  if (index < 0 || index >= this.size) {
+    return -1
+  }
+
+  let curIndex = 0
+  let cur = this.head
+  while (curIndex < index) {
+    cur = cur.next
+    curIndex++
+  }
+
+  return cur.val
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+  const node = new ListNode(val)
+  node.next = this.head
+  this.head = node.next
+  this.size++
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+  const node = new ListNode(val)
+  let cur = this.head
+  while (node.next) {
+    cur = next
+  }
+  cur.next = node
+  this.size++
+};
+
+/**
+ * @param {number} index
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+  if (this.size < index) {
+    return
+  }
+
+  index = Math.max(0, index)
+  let curIndex = 0
+  let cur = this.head
+  while (curIndex < index) {
+    cur = cur.next
+    curIndex++
+  }
+
+  const node = new ListNode(val)
+  node.next = cur.next
+  cur.next = node
+  this.size++
+};
+
+/**
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+  if (index >= this.size || index < 0) {
+    return
+  }
+
+  let curIndex = 0
+  let cur = this.head
+  while (curIndex < index) {
+    cur = cur.next
+    curIndex++
+  }
+
+  cur.next = cur.next.next
+  this.size--
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
--- 854-k-similar-strings/index.js
+++ 854-k-similar-strings/index.js
@@ -61,8 +61,61 @@ const swap = (cur, i, j) => {
 // };
 
 // dfs
-const kSimilarity = () => {
+var kSimilarity = function(s1, s2) {
+  let str1 = '';
+  let str2 = '';
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] !== s2[i]) {
+      str1 += s1[i];
+      str2 += s2[i];
+    }
+  }
+  if (str1.length === 0) {
+    return 0;
+  }
+  let ans = str1.length - 1;
 
+  const dfs = (pos, cost, len, str1, str2) => {
+    if (cost > ans) {
+      return;
+    }
+    while (pos < str1.length && str1[pos] === str2[pos]) {
+      pos++;
+    }
+    if (pos === str1.length) {
+      ans = Math.min(ans, cost);
+      return;
+    }
+    /* 当前状态的交换次数下限大于等于当前的最小交换次数 */
+    if (cost + minSwap(str1, str2, pos) >= ans) {
+      return;
+    }
+    for (let i = pos + 1; i < str1.length; i++) {
+      if (str1[i] === str2[pos]) {
+        const str1Next = swap(str1, i, pos);
+        dfs(pos + 1, cost + 1, len, str1Next, str2);
+      }
+    }
+  }
+
+  const minSwap = (s1, s2, pos) => {
+    let tot = 0;
+    for (let i = pos; i < s1.length; i++) {
+      tot += s1[i] !== s2[i] ? 1 : 0;
+    }
+    return Math.floor((tot + 1) / 2);
+  }
+
+  const swap = (cur, i, j) => {
+    const arr = [...cur];
+    const c = arr[i];
+    arr[i] = arr[j];
+    arr[j] = c;
+    return arr.join('');
+  }
+
+  dfs(0, 0, str1.length, str1, str2);
+  return ans;
 }
 
 console.log(kSimilarity(s1, s2))
