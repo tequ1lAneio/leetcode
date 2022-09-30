3d48df5bf02921800cabc1f7280e90854f790939
tequ1lAneio <evanlgp@hotmail.com>
2022-10-01 01:25:41 +0800

    added 01.08 interview

--- _interview/01.08-zero-matrix-lcci/index.js
+++ _interview/01.08-zero-matrix-lcci/index.js
@@ -10,11 +10,11 @@ matrix = [
   [1,0,1],
   [1,1,1]
 ]
-// matrix = [
-//   [0,1,2,0],
-//   [3,4,5,2],
-//   [1,3,1,5]
-// ]
+matrix = [
+  [0,1,2,0],
+  [3,4,5,2],
+  [1,3,1,5]
+]
 
 // simulate
 // const setZeroes = (matrix) => {
@@ -42,9 +42,89 @@ matrix = [
 //   return matrix
 // }
 
+// mark in first line&row
+// const setZeroes = (matrix) => {
+//   const m = matrix.length
+//   const n = matrix[0].length
+//   let colHasZero = false
+//   let rowHasZero = false
 //
-const setZeroes = (matrix) => {
+//   for (let i = 0; i < m; i++) {
+//     if (matrix[i][0] === 0) {
+//       colHasZero = true
+//       break
+//     }
+//   }
+//
+//   for (let i = 0; i < n; i++) {
+//     if (matrix[0][i] === 0) {
+//       rowHasZero = true
+//       break
+//     }
+//   }
+//
+//   for (let i = 1; i < m; i++) {
+//     for (let j = 1; j < n; j++) {
+//       if (matrix[i][j] === 0) {
+//         matrix[i][0] = 0
+//         matrix[0][j] = 0
+//       }
+//     }
+//   }
+//
+//   for (let i = 1; i < m; i++) {
+//     for (let j = 1; j < n; j++) {
+//       if (matrix[0][j] === 0 || matrix[i][0] === 0) {
+//         matrix[i][j] = 0
+//       }
+//     }
+//   }
+//
+//   if (colHasZero) {
+//     for (let i = 0; i < m; i++) {
+//       matrix[i][0] = 0
+//     }
+//   }
+//
+//   if (rowHasZero) {
+//     for (let i = 0; i < n; i++) {
+//       matrix[0][i] = 0
+//     }
+//   }
+//
+//   return matrix
+// }
 
+// single mark
+const setZeroes = (matrix) => {
+  const m = matrix.length
+  const n = matrix[0].length
+  let colHasZero = false
+
+  for (let i = 0; i < m; i++) {
+    if (matrix[i][0] === 0) {
+      colHasZero = true
+    }
+
+    for (let j = 1; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0
+        matrix[0][j] = 0
+      }
+    }
+  }
+
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = 1; j < n; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0
+      }
+    }
+
+    if (colHasZero) {
+      matrix[i][0] = 0
+    }
+  }
 }
 
 console.log(setZeroes(matrix))
