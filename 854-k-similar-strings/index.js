6a156dd6a40a1d0ad2da5ba0f9ed4fa668e80397
tequ1lAneio <evanlgp@hotmail.com>
2022-10-04 00:10:36 +0800

    updated 854

--- 854-k-similar-strings/index.js
+++ 854-k-similar-strings/index.js
@@ -6,8 +6,9 @@
 
 let s1, s2
 s1 = "ab", s2 = "ba"
-s1 = "abc", s2 = "bca"
+// s1 = "abc", s2 = "bca"
 s1 = "cadbacfeac", s2 = "baeacdacfc"
+s1 = "bccaba", s2 = "abacbc"
 
 const swap = (cur, i, j) => {
   const arr =  [...cur]
@@ -56,66 +57,66 @@ const swap = (cur, i, j) => {
 //     ans++
 //   }
 //
-//   console.log(visited)
 //   return ans
 // };
 
 // dfs
-var kSimilarity = function(s1, s2) {
-  let str1 = '';
-  let str2 = '';
+const kSimilarity = (s1, s2) => {
+  let str1 = ''
+  let str2 = ''
+
   for (let i = 0; i < s1.length; i++) {
     if (s1[i] !== s2[i]) {
-      str1 += s1[i];
-      str2 += s2[i];
+      str1 += s1[i]
+      str2 += s2[i]
     }
   }
-  if (str1.length === 0) {
-    return 0;
-  }
-  let ans = str1.length - 1;
 
-  const dfs = (pos, cost, len, str1, str2) => {
+  if (str1.length === 0) {
+    return 0
+  }
+
+  let ans = str1.length - 1
+
+  const dfs = (pos, cost, str1, str2) => {
     if (cost > ans) {
-      return;
+      return
     }
+
     while (pos < str1.length && str1[pos] === str2[pos]) {
-      pos++;
+      pos++
     }
+
     if (pos === str1.length) {
-      ans = Math.min(ans, cost);
-      return;
+      ans = Math.min(ans, cost)
+      return
     }
-    /* 当前状态的交换次数下限大于等于当前的最小交换次数 */
+
     if (cost + minSwap(str1, str2, pos) >= ans) {
-      return;
+      return
     }
-    for (let i = pos + 1; i < str1.length; i++) {
-      if (str1[i] === str2[pos]) {
-        const str1Next = swap(str1, i, pos);
-        dfs(pos + 1, cost + 1, len, str1Next, str2);
+
+    for (let i = 0; i < str1.length; i++) {
+      if (str2[pos] === str1[i]) {
+        const next = swap(str1, i, pos)
+        dfs(pos + 1, cost + 1, next, str2)
       }
     }
   }
 
-  const minSwap = (s1, s2, pos) => {
-    let tot = 0;
-    for (let i = pos; i < s1.length; i++) {
-      tot += s1[i] !== s2[i] ? 1 : 0;
+  const minSwap = (str1, str2, pos) => {
+    let total = 0
+    for (let i = pos; i < str1.length; i++) {
+      if (str1[i] !== str2[i]) {
+        total++
+      }
     }
-    return Math.floor((tot + 1) / 2);
+
+    return Math.floor((total + 1) / 2)
   }
 
-  const swap = (cur, i, j) => {
-    const arr = [...cur];
-    const c = arr[i];
-    arr[i] = arr[j];
-    arr[j] = c;
-    return arr.join('');
-  }
-
-  dfs(0, 0, str1.length, str1, str2);
-  return ans;
+  dfs(0, 0, str1, str2)
+  return ans
 }
 
 console.log(kSimilarity(s1, s2))
