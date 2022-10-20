95a7abba1b6065f11970633d1be9bc6c12dac1a4
tequ1lAneio <evanlgp@hotmail.com>
2022-10-20 23:10:01 +0800

    updated 940

--- 940-distinct-subsequences-ii/index.js
+++ 940-distinct-subsequences-ii/index.js
@@ -11,36 +11,27 @@ s = "abb"
 
 
 const distinctSubseqII = (s) => {
-  const n = s.length
-  const modulo = 1000000007
+  const MOD = 1000000007;
+  const last = new Array(26).fill(-1);
 
-  for (let i = 0; i < n; i++) {
-
+  const n = s.length;
+  const f = new Array(n).fill(1);
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < 26; ++j) {
+      if (last[j] !== -1) {
+        f[i] = (f[i] + f[last[j]]) % MOD;
+      }
+    }
+    last[s[i].charCodeAt() - 'a'.charCodeAt()] = i;
   }
-}
 
-// var distinctSubseqII = function(s) {
-//   const MOD = 1000000007;
-//   const last = new Array(26).fill(-1);
-//
-//   const n = s.length;
-//   const f = new Array(n).fill(1);
-//   for (let i = 0; i < n; ++i) {
-//     for (let j = 0; j < 26; ++j) {
-//       if (last[j] !== -1) {
-//         f[i] = (f[i] + f[last[j]]) % MOD;
-//       }
-//     }
-//     last[s[i].charCodeAt() - 'a'.charCodeAt()] = i;
-//   }
-//
-//   let ans = 0;
-//   for (let i = 0; i < 26; ++i) {
-//     if (last[i] !== -1) {
-//       ans = (ans + f[last[i]]) % MOD;
-//     }
-//   }
-//   return ans;
-// };
+  let ans = 0;
+  for (let i = 0; i < 26; ++i) {
+    if (last[i] !== -1) {
+      ans = (ans + f[last[i]]) % MOD;
+    }
+  }
+  return ans;
+}
 
 console.log(distinctSubseqII(s))
