1944043a58fd3cb363371a3108ee312e9b5c38c7
guangpengliu-vungle <guangpeng.liu@vungle.com>
2022-11-07 14:07:59 +0800

    updated 003

--- 3-longest-substring-without-repeating-characters/index.js
+++ 3-longest-substring-without-repeating-characters/index.js
@@ -3,13 +3,40 @@
  * @return {number}
  */
 
-const s = 'abcabcbb'
+let s
+s = 'abcabcbb'
+s = "bbbbb"
+// s = "pwwkew"
 
-const lengthOfLongestSubstring = function(s) {
-  let pointer = 0
-  let length = s.length
+// const lengthOfLongestSubstring = (s) => {
+//   const n = s.length
+//   let ans = 0
+//
+//   for (let i = 0; i < n; i++) {
+//     const map = new Map()
+//     for (let j = i; j < n; j++) {
+//       if (map.has(s[j])) {
+//         break
+//       } else {
+//         map.set(s[j], 1)
+//         ans = Math.max(ans, j - i + 1)
+//       }
+//     }
+//   }
+//
+//   return ans
+// }
 
-  for (let i = 0; i < s.length; i++) {
+const lengthOfLongestSubstring = (s) => {
+  const n = s.length
+  const set = new Set()
+
+  let p = 0
+  let ans = 1
+
+  for (let i = 0; i < n; i++) {
 
   }
-};
+}
+
+console.log(lengthOfLongestSubstring(s))
