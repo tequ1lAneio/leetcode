ef86ed415ff26666d63864e3dc0fd2d7f89f6a82
guangpengliu-vungle <guangpeng.liu@vungle.com>
2023-01-19 08:25:58 +0800

    added 1739

--- 1739-building-boxes/index.js
+++ 1739-building-boxes/index.js
@@ -5,9 +5,28 @@
 
 let n
 n = 3
-n = 4
-n = 10
+// n = 4
+// n = 10
 
 const minimumBoxes = (n) => {
+  let cur = 1
+  let i = 1
+  let j = 1
 
+  while (n > cur) {
+    n -= cur
+    i++
+    cur += i
+  }
+
+  cur = 1
+  while (n > cur) {
+    n -= cur
+    j++
+    cur++
+  }
+
+  return (i - 1) * i / 2 + j
 }
+
+console.log(minimumBoxes(n))
